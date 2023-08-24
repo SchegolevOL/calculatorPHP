260aa6e4487c71e5e431042a9732af18a78d134c
Oleg Schegol <olschegolev@yahoo.com>
2023-08-24 14:22:19 +0500

    working program

--- index.php
+++ index.php
@@ -14,20 +14,43 @@
 <?php error_reporting(-1);
 $res=0;
 $err='';
-print_r($_GET);
-if($_GET['number_left']=='')$err="the left number is not known";
+
+//if (!is_float($_GET['left_ampersand'])) {
+//    $err = 'the left ampersand is not valid';
+//
+//}
+//elseif (!is_float($_GET['right_ampersand'])) {
+//    $err = 'the right ampersand is not valid';
+//}
+//else {
+if (isset($_GET['action'])!=null){
+    $res = match ($_GET['action']) {
+        'addition' => floatval($_GET['left_ampersand']) + floatval($_GET['right_ampersand']),
+        'subtraction' => floatval($_GET['left_ampersand']) - floatval($_GET['right_ampersand']),
+        'multiplication' => floatval($_GET['left_ampersand']) * floatval($_GET['right_ampersand']),
+        'division' => floatval($_GET['left_ampersand']) / floatval($_GET['right_ampersand'])
+    };
+}
+
+
+
+//}
+
+//if($_GET['number_left']==''&&$_GET['number_right']!='')$err="the left ampersand is not known";
+//if(!$_GET['number_left']==''&&$_GET['number_right']=='')$err="the right ampersand is not known";
+
 ?>
 
 <div class="input-group mb-3">
     <form action="" method="get">
-        <input type="text" name="number_left" placeholder="Enter number left"class="">
+        <input type="text" name="left_ampersand" placeholder="Enter number left"class="">
         <select name="action" id="" class="form-select-sm text-center">
             <option value="addition">+</option>
             <option value="subtraction">-</option>
             <option value="multiplication">*</option>
             <option value="division">/</option>
         </select>
-        <input type="text" name="number_right" placeholder="Enter number right" left"class="form-control">
+        <input type="text" name="right_ampersand" placeholder="Enter number right" left"class="form-control">
         <button type="submit" class="btn btn-primary">=</button>
         <lable class="focus-ring"><?=$res?></lable>
     </form>
